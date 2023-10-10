b6eac2dacabfdf385a6cfde984745b883f37d250
mkilicarslankarapinar <mkilicarslankarapinar@gmail.com>
2023-10-10 15:04:43 +0300

    Angular Porject Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './Components/product/product.component';
+import { CategoryComponent } from './Components/category/category.component';
+import { NaviComponent } from './Components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
